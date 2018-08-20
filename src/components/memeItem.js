070d5bf934af5b6f863e9483b31573feac6207fb
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2018-08-19 19:47:47 -0700

    completed full CRUD

--- src/components/memeItem.js
+++ src/components/memeItem.js
@@ -8,6 +8,14 @@ class MemeItem extends Component {
     });
   }
 
+  updateMeme() {
+    axios
+      .put(`/api/memes/${this.props.meme.id}`, { title: this.refs.title.value })
+      .then(memes => {
+        this.props.setMemes(memes.data);
+      });
+  }
+
   render() {
     return (
       <li className="meme">
@@ -15,6 +23,8 @@ class MemeItem extends Component {
         <img src={this.props.meme.url} />
         {this.props.meme.title}
         <button onClick={this.deleteMeme.bind(this)}>Delete Meme</button>
+        <input type="text" ref="title" />
+        <button onClick={this.updateMeme.bind(this)}>Update</button>
       </li>
     );
   }
